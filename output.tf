ced9aa14890046dee9ffd0dfa9c720224a053f10
Markus Korn <koma0815@googlemail.com>
2018-04-24 15:13:08 +0200

    New outputs (#12)
    * fix path and proxy parameter handling
    
    * added autoscaling:DescribeLaunchConfigurations to iam policy
    
    * added output

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "security_group_nodes_id" {
 output "security_group_master_id" {
   value = "${aws_security_group.master.id}"
 }
+
+output "cluster_role_name" {
+  value = "${aws_iam_role.cluster.name}"
+}

9830d5e60cdaba7ba2490eaf1453da3339f775cb
Barthelemy Benoit (uia61219) <uia61219@tslinfdu.conti.de>
2018-07-13 17:16:47 +0200

    Added input description

--- input.tf
+++ input.tf
@@ -1,5 +1,10 @@
-variable "name" {}
-variable "ssh_key" {}
+variable "name" {
+  description = "The cluster name"
+}
+
+variable "ssh_key" {
+  description = "The name of SSH key to use"
+}
 
 variable "cluster_network_plugin" {
   description = "The Pod networking CNI network plugin : flannel(default) / weave"
@@ -7,46 +12,67 @@ variable "cluster_network_plugin" {
 }
 
 variable "master_instance_type" {
-  default = "t2.small"
+  description = "The Master instance type"
+  default     = "t2.small"
 }
 
 variable "node_instance_type" {
-  default = "t2.large"
+  description = "The node instance type"
+  default     = "t2.large"
 }
 
-variable "node_asg_min" {}
-variable "node_asg_max" {}
-variable "node_asg_desired" {}
-variable "vpc" {}
+variable "node_asg_min" {
+  description = "The minimal size of the node autoscaling group"
+}
+
+variable "node_asg_max" {
+  description = "The maximum size of the node autoscaling group"
+}
+
+variable "node_asg_desired" {
+  description = "The initial size of the node autoscaling group"
+}
+
+variable "vpc" {
+  description = "The ID of the VPC containing the cluster"
+}
 
 variable "proxy_servers" {
-  default = ""
+  description = "The DNS name of the proxy server with port <ip>:port"
+  default     = ""
 }
 
 variable "subnets" {
-  default = []
+  description = "An array containing the subnets to spawn the cluster members in."
+  default     = []
 }
 
 variable "kubernetes_version" {
-  default = "1.8.7"
+  description = "The kubernetes version"
+  default     = "1.8.7"
 }
 
 variable "kubernetes_dashboard_version" {
-  default = "1.8.2"
+  description = "The Kubernetes Dashboard version"
+  default     = "1.8.2"
 }
 
 variable "additional_iam_policy" {
-  default = ""
+  description = "The content of an additionnal IAM policy"
+  default     = ""
 }
 
 variable "additional_certificates" {
-  default = ""
+  description = "Additional certificates put on the nodes, useful if you have private docker repositories"
+  default     = ""
 }
 
 variable "additional_tags" {
-  default = {}
+  description = "Additionnal custom tags, e.g. for corporate billing"
+  default     = {}
 }
 
 variable "enable_kube2iam" {
-  default = true
+  description = "Enable Kube2IAM to add security to AWS API access from pods"
+  default     = true
 }

2e90a662e9c5bab0e69975b40d37c144e85f3d74
Barthelemy Benoit (uia61219) <uia61219@tslinfdu.conti.de>
2018-07-13 14:51:45 +0200

    reformatted using terraform fmt

--- autoscaling_master.tf
+++ autoscaling_master.tf
@@ -11,24 +11,24 @@ data "template_file" "master" {
 
   vars {
     s3_id = "${aws_s3_bucket.cluster.id}"
-    role = "master"
+    role  = "master"
 
-    proxy = "${replace("${var.proxy_servers}", ",", " ")}"
-    volume = "${aws_ebs_volume.master.id}"
+    proxy             = "${replace("${var.proxy_servers}", ",", " ")}"
+    volume            = "${aws_ebs_volume.master.id}"
     load_balancer_dns = "${aws_lb.master.dns_name}"
   }
 }
 
 resource "aws_ebs_volume" "master" {
-    availability_zone = "${data.aws_subnet.region_1a.availability_zone}"
-    size = 40
-    encrypted = true
-    type = "gp2"
-    tags = "${merge(var.additional_tags, map("Name", "${var.name}-master"))}"
+  availability_zone = "${data.aws_subnet.region_1a.availability_zone}"
+  size              = 40
+  encrypted         = true
+  type              = "gp2"
+  tags              = "${merge(var.additional_tags, map("Name", "${var.name}-master"))}"
 }
 
 resource "aws_security_group" "master" {
-  name_prefix        = "${var.name}-master"
+  name_prefix = "${var.name}-master"
   description = "${var.name}-master"
   vpc_id      = "${var.vpc}"
 
@@ -47,37 +47,37 @@ resource "aws_security_group" "master" {
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = ["${aws_security_group.nodes.id}"]
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0  
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
 
     self = true
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = "${var.additional_tags}"
 }
 
 resource "aws_launch_configuration" "master" {
-  name_prefix      = "${var.name}-master-"
-  image_id         = "${data.aws_ami.ubuntu.id}"
-  instance_type    = "${var.master_instance_type}"
-  security_groups  = ["${aws_security_group.master.id}"]
-  key_name         = "${var.ssh_key}"
-  user_data        = "${data.template_file.master.rendered}"
+  name_prefix          = "${var.name}-master-"
+  image_id             = "${data.aws_ami.ubuntu.id}"
+  instance_type        = "${var.master_instance_type}"
+  security_groups      = ["${aws_security_group.master.id}"]
+  key_name             = "${var.ssh_key}"
+  user_data            = "${data.template_file.master.rendered}"
   iam_instance_profile = "${aws_iam_instance_profile.cluster.id}"
 
   root_block_device {
@@ -87,7 +87,7 @@ resource "aws_launch_configuration" "master" {
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes = [ "image_id" ]
+    ignore_changes        = ["image_id"]
   }
 }
 
@@ -101,7 +101,6 @@ resource "aws_autoscaling_group" "master" {
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.master.name}"
   vpc_zone_identifier       = ["${data.aws_subnet.region_1a.id}"]
-
   target_group_arns         = ["${aws_lb_target_group.master_443.arn}"]
 
   tags = ["${concat(
@@ -118,11 +117,11 @@ resource "aws_autoscaling_group" "master" {
 }
 
 resource "aws_lb" "master" {
-  name = "${var.name}-master"
-  internal        = true
-  subnets         = ["${data.aws_subnet.region_1a.id}"]
+  name               = "${var.name}-master"
+  internal           = true
+  subnets            = ["${data.aws_subnet.region_1a.id}"]
   load_balancer_type = "network"
-  
+
   tags = "${merge(var.additional_tags, map("Name", "${var.name}-master"))}"
 }
 
